2f97a71378c4a091754bb17cadeac959daec68be
Jay Vigilla <JayRVigilla@gmail.com>
2020-03-25 12:33:48 -0700

    creating @app.routes, finished /users

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -11,3 +11,26 @@ app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
 db.create_all()
 
+
+@app.route('/')
+def get_index():
+    """ Goes to list of Users """
+    return redirect("/users")
+
+
+@app.route('/users')
+def show_users():
+    """ List of all Users """
+    users = User.query.all()
+    return render_template(
+        'userlisting.html',
+        users=users
+    )
+
+
+@app.route()
+# @app.route()
+# @app.route()
+# @app.route()
+# @app.route()
+# @app.route()
