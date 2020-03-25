2b462ac174f8744123417a03c491bd894e98b7dc
Jay Vigilla <JayRVigilla@gmail.com>
2020-03-25 12:20:37 -0700

    set up pages and basics

--- models.py
+++ models.py
@@ -1,1 +1,31 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+def User(db.Model)
+""" Class for User"""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.String,
+                    nullable=False)
+    last_name = db.Column(db.String,
+                    nullable=False)
+    # TODO They can have their annonymity,
+    # but we want a default image if None
+    # https://www.cfdating.com/user_images/default.png
+    image_url = db.Column(
+        db.String,
+        default='https://www.cfdating.com/user_images/default.png')
