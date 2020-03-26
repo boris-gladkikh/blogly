92e458b954695d1860f4906d132f2b0d4ce24d32
gladkill <gladkillmusic@gmail.com>
2020-03-25 20:31:20 -0700

    boris commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from models import db, connect_db, User
 
 app = Flask(__name__)
@@ -15,10 +15,10 @@ db.create_all()
 @app.route('/')
 def get_index():
     """ Goes to list of Users """
-    return redirect("/users")
+    return redirect("/users/")
 
 
-@app.route('/users')
+@app.route('/users/')
 def show_users():
     """ List of all Users """
     users = User.query.all()
@@ -28,14 +28,49 @@ def show_users():
     )
 
 
-@app.route('users/new')
-def create_new_user();
+@app.route('/users/new/')
+def create_new_user():
     """ Takes form info and creates new user """
+    return render_template('newuserform.html')
 
 
+@app.route('/users/new/', methods=["POST"])
+def post_new_user():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    image_url = request.form["image_url"] or None
 
-# @app.route()
-# @app.route()
-# @app.route()
+
+    new_user = User(first_name = first_name,
+    last_name = last_name,
+    image_url = image_url)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users/")
+
+    
+@app.route('/users/<user_id>/')
+def info_about_user(user_id):
+
+    current_user = User.query.filter_by(id=user_id).one()
+    user_full_name = f"{current_user.first_name} {current_user.last_name}"
+    user_image = current_user.image_url
+
+    print("\n\n\n This is our user_image \n\n", user_image)
+
+
+    return render_template('userdetails.html',
+    user = user_full_name,
+    image_url = user_image,
+    user_id = user_id
+    )
+
+
+@app.route('/users/<user_id>/edit/')
+def edit_user(user_id):
+
+    return render_template('useredit.html')
 # @app.route()
 # @app.route()
